34ed914e1d9b5d0f6badbbf03ceee17ba1231e9a
odupree1988 <odupree1988@gmail.com>
2021-07-13 10:39:26 -0400

    pw

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: "root",
   // Your MySQL password
-  password: "Tleb15*1508*1wd",
+  password: "",
   database: "election",
 });
 
